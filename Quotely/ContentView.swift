3fce00720d67c706d4d9d022567575efa2cde84a
Jchillah <jchillah@gmail.com>
2025-01-11 23:22:54 +0100

    Initial Commit

---
+++ Quotely/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quotely
+//
+//  Created by Michael Winkler on 11.01.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quotely/QuotelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotelyApp.swift
+//  Quotely
+//
+//  Created by Michael Winkler on 11.01.25.
+//
+
+import SwiftUI
+
+@main
+struct QuotelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
